4fb371b3fa3b5d55f89c9b0e8a0e9b084b91bef7
prasad <prasadvanaparthy@gmail.com>
2017-03-14 10:23:23 +0530

    added mobile

--- contact.php
+++ contact.php
@@ -87,6 +87,7 @@ return true;
 <tr><td>Name: <input type="text" name="first_name"></td></tr>
 <tr><td>Email: <input type="text" name="email"></td></tr>
 <tr><td>Phone: <input type="text" name="phone"></td></tr>
+<tr><td>Mobile: <input type="text" name="mphone"></td></tr>
 <tr><td><input type="submit" name="submit">
 
   <!--  <input  type="submit" value="Register" name="Register"> -->
